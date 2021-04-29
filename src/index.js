1021fe5fbf6d5545e6db3833a31b687255d5000b
Preeti Cyril Mammen <preeticyril@MacBook-Pro-2.local>
2021-04-29 14:47:40 +1000

    new changes

--- src/index.js
+++ src/index.js
@@ -2,53 +2,70 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstbook = {
-  image:
-    "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL640_FMwebp_QL65_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "by Amelia Hepworth and Tim Warnes",
-};
-
-const secondbook = {
-  image:
-    "https://m.media-amazon.com/images/I/81IH8kx0PgL._AC_UY436_FMwebp_QL65_.jpg",
-  title: "The Dreaded Groak: Overcoming prejudice and discrimination",
-  author: "by Peta Lemon and Maria Dasic Todoric",
-};
+// Object Array for books
+const books = [
+  {
+    id: 1,
+    image:
+      "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "by Amelia Hepworth and Tim Warnes",
+  },
+  {
+    id: 2,
+    image:
+      "https://m.media-amazon.com/images/I/81IH8kx0PgL._AC_UY436_FMwebp_QL65_.jpg",
+    title: "The Dreaded Groak: Overcoming prejudice and discrimination",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+  {
+    id: 3,
+    title: "Lady with a red umbrella",
+    image: "https://picsum.photos/seed/picsum/200/200",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+  {
+    id: 4,
+    title: "Flowers and some fruits",
+    image: "https://picsum.photos/seed/picsum/200/200",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+  {
+    id: 5,
+    title: "Beautiful scenery",
+    image: "https://picsum.photos/seed/picsum/200/200",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+  {
+    id: 6,
+    title: "Some kind of bird",
+    image: "https://picsum.photos/seed/picsum/200/200",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+  {
+    id: 7,
+    title: "The attack of dragons",
+    image: "https://picsum.photos/seed/picsum/200/200",
+    author: "by Peta Lemon and Maria Dasic Todoric",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        title={firstbook.title}
-        author={firstbook.author}
-        image={firstbook.image}
-      >
-        {/* Children Prop */}
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-          laborum ut soluta deserunt, deleniti rem dolor? Excepturi ipsam beatae
-          inventore nihil impedit temporibus a officia, dolorum numquam
-          perspiciatis, aut explicabo.
-        </p>
-      </Book>
-      <Book
-        title={secondbook.title}
-        author={secondbook.author}
-        image={secondbook.image}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { image, title, author, children } = props;
+const Book = ({ image, title, author }) => {
   return (
     <article className="book">
       <img src={image} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
